f3131c90c25df280cea84a9f15c5ac9af82e9993
Maxim Novikov <maxim_novikov1@epam.com>
2021-09-23 01:31:51 +0300

    feat: add copy and delete from bucket

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -9,36 +9,45 @@ import * as csv from 'csv-parser';
 
 const handler = async (event: S3Event) => {
   const s3 = new S3({ region: GLOBAL_INFO.REGION });
-  const result = [];
+  const result: Record<string, unknown>[] = [];
 
   try {
     for (let record of event.Records) {
       const { key } = record.s3.object;
       console.log(`Start reading '${key}' from bucket '${GLOBAL_INFO.CSV_BUCKET}'`)
-      await new Promise((res, rej): void => {
+      await new Promise((res: (value: unknown) => void, rej: (error?: Error) => void): void => {
         const readableStream = s3.getObject({ Bucket: GLOBAL_INFO.CSV_BUCKET, Key: key }).createReadStream();
         readableStream
           .pipe(csv())
-          .on('data', (data) => {
+          .on('data', (data: Record<string, unknown>) => {
             result.push(data);
             console.log('CSV Parser did it --->', data);
           })
-          .on('err', (err) => {
+          .on('err', (err: Error) => {
             console.error(`Error occurred while reading data from bucket '${GLOBAL_INFO.CSV_BUCKET}'. ${err}`);
             rej(err);
           })
-          .on('end', () => {
+          .on('end', async () => {
             console.log(`End of reading '${key}' from bucket '${GLOBAL_INFO.CSV_BUCKET}'`);
-            res('');
+            await s3.copyObject({
+              Bucket: GLOBAL_INFO.CSV_BUCKET,
+              CopySource: `${GLOBAL_INFO.CSV_BUCKET}/${key}`,
+              Key: key.replace(GLOBAL_INFO.UPLOADED, GLOBAL_INFO.PARSED)
+            }).promise();
+
+            await s3.deleteObject({
+              Bucket: GLOBAL_INFO.CSV_BUCKET,
+              Key: key
+            }).promise();
           })
-      })
+      });
     }
 
     return formatJSONResponse({
       statusCode: 200,
       response: result,
     });
-  } catch (err: any) {
+  } catch (err: unknown) {
     return formatJSONResponse({
       response: err,
       statusCode: 500,
